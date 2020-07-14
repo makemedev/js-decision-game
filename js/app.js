fcaec492fac15b68c82904025fda61fb3cd48103
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2020-07-13 21:53:44 -0500

    init commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/*
+    INTERACTIVE LOGIC GAME
+    Build a game where a user gets presented decisions and, based on the user's choice, a storyline of your choosing develops.
+    Requirements:  
+    1. Use the `window.prompt` and/or `window.confirm` method to let users make decisions in your story.
+    3. Users should have to make at least 3 decisions. Each decision should affect the outcome of your story.
+    4. Upon the final decision, Your html page should update, displaying the result of the user's decisions. 
+        - There is a div with the id `result` in the html document. 
+        - Create an html element containing the game's result (text) and add the element as a child to #result
+        - Once your result is appended to the DOM, target #result, remove the .hide class, 
+            and add the .fade-in-up class to see the user's result appear on the screen.
+    
+    Helpful information:
+    window.prompt - https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
+    window.confirm - https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm
+    if/elseif/else - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+*/
+
+// Your code starts here
